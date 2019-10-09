c6d4652a0d91e9bbee3610aada73d1658d79b663
akshitha_husky <shetty.aks@husky.neu.edu>
2019-10-09 10:09:58 +0530

    changes

---
+++ main.js
@@ -0,0 +1,55 @@
+function openModal1() {
+  var modal = document.querySelector(".modal1");
+  var modalBg = document.querySelector(".modalBackground");
+  modalBg.style.display ="block";
+  modal.style.display="block";
+}
+
+function closeModal1(){
+  var modal = document.querySelector(".modal1");
+  var modalBg = document.querySelector(".modalBackground");
+  modalBg.style.display ="none";
+  modal.style.display="none";
+}
+
+function openModal2() {
+  var modal = document.querySelector(".modal2");
+  var modalBg = document.querySelector(".modalBackground");
+  modalBg.style.display ="block";
+  modal.style.display="block";
+}
+
+function closeModal2(){
+  var modal = document.querySelector(".modal2");
+  var modalBg = document.querySelector(".modalBackground");
+  modalBg.style.display ="none";
+  modal.style.display="none";
+}
+
+function openModal3() {
+  var modal = document.querySelector(".modal3");
+  var modalBg = document.querySelector(".modalBackground");
+  modalBg.style.display ="block";
+  modal.style.display="block";
+}
+
+function closeModal3(){
+  var modal = document.querySelector(".modal3");
+  var modalBg = document.querySelector(".modalBackground");
+  modalBg.style.display ="none";
+  modal.style.display="none";
+}
+
+function openModal4() {
+  var modal = document.querySelector(".modal4");
+  var modalBg = document.querySelector(".modalBackground");
+  modalBg.style.display ="block";
+  modal.style.display="block";
+}
+
+function closeModal4(){
+  var modal = document.querySelector(".modal4");
+  var modalBg = document.querySelector(".modalBackground");
+  modalBg.style.display ="none";
+  modal.style.display="none";
+}
